70f6cd8d2ab2c8153ac529d1b4aa2918892883fa
Ujjwal Gupta <ujjwal.kbr@gmail.com>
2020-07-22 17:10:37 +0530

    Add cors config

---
+++ src/main/java/com/fancyfrog/reddit/config/WebConfig.java
@@ -0,0 +1,26 @@
+package com.fancyfrog.reddit.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by Ujjwal Gupta on Jul,2020
+ */
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .maxAge(3600L)
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization")
+                .allowCredentials(true);
+    }
+}
